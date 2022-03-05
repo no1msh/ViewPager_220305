4a3062a01bb3460f5794f628e167835defc339f3
TFQ_Moon <no1msh1217@gmail.com>
2022-03-05 18:54:26 +0900

    ViewPagerAdapter

--- app/src/main/java/com/example/viewpager_220305/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/example/viewpager_220305/adapters/MainViewPagerAdapter.kt
@@ -11,33 +11,24 @@ class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
     override fun getPageTitle(position: Int): CharSequence? {
 
-        if (position == 0) {
-            return "이름"
+        return when (position) {
+            0 -> return "이름"
+            1 -> return "내 정보"
+            else -> return "인사말"
         }
-        else if (position == 1){
-            return "내 정보"
-        }
-        else {
-            return "인사말"
-        }
+
     }
 
-    override fun getCount(): Int {
-        return 3
-    }
+    override fun getCount() = 3
+
 
     override fun getItem(position: Int): Fragment {
 
-        if (position == 0) {
-            return MyNameFragment()
+        return when (position) {
+            0 -> MyNameFragment()
+            1 -> MyInfoFragment()
+            else -> MyHelloFragment()
         }
 
-        else if (position == 1) {
-            return MyInfoFragment()
-        }
-
-        else {
-            return MyHelloFragment()
-        }
     }
 }
